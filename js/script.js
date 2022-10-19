e49e2a1b66e282a3b59967b3b42c4caad6807de0
Naza Zulfiqi <zulfiqinaza@gmail.com>
2022-10-19 13:29:25 +0700

    selesai login register html css

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let nilaiAkhir = 40
+
+if (nilaiAkhir > 80 && nilaiAkhir <= 100){
+    console.log('Sangat Memuaskan')
+}else if(nilaiAkhir >= 70 && nilaiAkhir < 80){
+    console.log('Memuaskan')
+}else{
+    console.log('Cukup');
+}
